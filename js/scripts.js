809457033a2bd1a1dac074f36abd06e1fe938768
Firthuns2021 <86574425+Firthuns2021@users.noreply.github.com>
2022-04-05 18:55:12 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+console.log('funciona correctamente');
+
+//1- querySelector
+const heading = document.querySelector('.header__texto h2') // 0 o 1 Elementos
+console.log(heading)
+
+const headingID = document.querySelector('#heading') // 0 o 1 Elementos
+console.log(headingID)
+
+//2- querySelectorAll
+
+
+
+
+//3- getElementById
